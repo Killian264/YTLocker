9eac8f759611707805e7a7cb2109bcff01efb0ea
Killian Debacker <killiandebacker@gmail.com>
2020-12-24 22:13:45 -0600

    remove tracking on uneeded files

--- cool.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    console.info("EVENT\n" + JSON.stringify(event, null, 2))
-  console.log("query string params ", event.queryStringParameters["hub.challenge"])
-  return {
-      'hub_challenge': event.queryStringParameters["hub.challenge"],
-  };
-};
