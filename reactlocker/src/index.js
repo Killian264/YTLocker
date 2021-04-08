71e6f82a7b79037eb69d8ce45f04c64eb2ce6fd6
Killian Debacker <killiandebacker@gmail.com>
2021-04-07 21:25:22 -0500

    add tailwindcss

--- reactlocker/src/index.js
+++ reactlocker/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import './styles/main.css'
+import Base from './components/Base';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Base />
   </React.StrictMode>,
   document.getElementById('root')
 );
