77b8e1611b4526a97f910a38dd0cf590d8987385
Killian Debacker <killiandebacker@gmail.com>
2021-05-09 18:06:17 -0500

    add typescript

--- reactlocker/src/components/Alert.tsx
+++ reactlocker/src/components/Alert.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+interface Props {
+	className: string;
+	type: keyof typeof colors;
+}
 
 const colors = {
 	success: "text-accent-text bg-green-400",
 	failure: "text-accent-text bg-red-400",
 }
 
-
-export const Alert = ({ className, children, type="success", ...props }) => {
+export const Alert: React.FC<Props> = ({ className, type="success", children, ...props }) => {
 
 	let size = "md:w-9/12 sm:w-full"
 	let pos = "z-10 fixed inset-x-0 mx-auto mt-3"
@@ -16,16 +19,10 @@ export const Alert = ({ className, children, type="success", ...props }) => {
 
 	return (
 		<div
-			type="button"
 			className={`flex justify-center py-2 px-6 rounded-lg text-sm font-bold ${css}`}
 			{...props}
 		>
 			<span>{children}</span>
 		</div>
 	);
-};
-
-Alert.propTypes = {
-	type: PropTypes.oneOf(['success', 'failure']),
-	className: PropTypes.string,
 };
